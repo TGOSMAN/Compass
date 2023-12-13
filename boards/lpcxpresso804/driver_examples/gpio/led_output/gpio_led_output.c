085cd1d7080e81b51bfc032f4028d61d624012a8
TGOSMAN <thomasgosman03@outlook.com>
2023-12-13 11:16:53 +1100

    Put SWM initialisation back into the initPWM so that the output will come out on pin8, NOTE for testing PWM duty is at 10%

--- boards/lpcxpresso804/driver_examples/gpio/led_output/gpio_led_output.c
+++ boards/lpcxpresso804/driver_examples/gpio/led_output/gpio_led_output.c
@@ -6,6 +6,7 @@
 #define	CTIMER  (0x40038000)
 #define GPIO    (0xA0000000)
 #define SYSCON  (0x40048000)
+#define SWM		(0x4000C000)
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
@@ -25,12 +26,35 @@ This Function initialises the GPIO Port by:
     -   Clears the Preset on the GPIO port to operate
 Parameters:
 -   None
+Return: 
+- 	Nothing
 */
 void initPort(void) { 
 	volatile uint32_t *AHBCLK = (volatile uint32_t *) (SYSCON+0x80);//AHBCLK Control Register
 	volatile uint32_t *PRESET0 = (volatile uint32_t *) (SYSCON+0x88);//Port Presets Control Register
 	*AHBCLK |= (1<<6); // enable port clock
 	*PRESET0 |= (1<<6);
+	return;
+}
+
+/////////////////////////////Brief/////////////////////////////
+/*
+Description:
+This Function initialises the GPIO Port by:
+    -   Turns on the AHB clock to the GPIO port
+    -   Clears the Preset on the GPIO port to operate
+Parameters:
+-   None
+Return: 
+- 	Nothing
+*/
+void changeDIR(uint32_t PIN, int direction){
+	volatile uint32_t *DIR = (volatile uint32_t *) (GPIO+0x2000);//AHBCLK Control Register
+	if (direction){
+		*DIR |= (1<<PIN);
+	} else {
+		*DIR &= ((0x7FFFFFFF)^(1<<PIN));
+	}
 	return;
 }
 /////////////////////////////Brief/////////////////////////////
@@ -44,6 +68,8 @@ This function initialises the PWM Feature by
     -   Enables the internal PWM register on MR0
 Parameters:
 -   None -> maybe add frequency
+Return: 
+- 	Nothing
 */
 void initPWM(void) {
     volatile uint32_t *AHBCLK = (volatile uint32_t *) (SYSCON+0x80);//AHBCLK Control Register
@@ -53,8 +79,10 @@ void initPWM(void) {
     volatile uint32_t *MR0 = (volatile uint32_t *) (CTIMER+0x18);
     volatile uint32_t *MR3 = (volatile uint32_t *) (CTIMER+0x24);
     volatile uint32_t *PWMC = (volatile uint32_t *) (CTIMER+0x74);
+    volatile uint32_t *PINASSIGN4 = (volatile uint32_t *) (SWM+0x10);
     //volatile uint32_t *PRESET0 = (volatile uint32_t *) 0x40048088;//Port Presets Control Register
 	*AHBCLK |= (1<<25);// Set AHB clock on for CTIMER
+	*AHBCLK |= (1<<7);// Set AHB clock on for SWM
 
     // Configure timer
     *TCR = 0x02;    // Reset timer
@@ -63,13 +91,14 @@ void initPWM(void) {
 
     // Configure match control register (reset on MR3, interrupt)
     *MCR = (1 << 10) | (1 << 9) ;
-    // Set duty cycle (e.g., 0%)
-    *MR0 = 999;
-    // Enable PWM on MR0
-    *PWMC = 0x01;
+    *MR0 = 900;// Set duty cycle (e.g., 0%)
+    *PWMC = 0x01;// Enable PWM on MR0
 		
     // Start the timer
     *TCR = 0x01;
+
+	//Assign the output to P0_8
+	*PINASSIGN4 &= 0x8; 
 	return;
 }
 /*!
@@ -85,10 +114,13 @@ int main(void)
 		//GPIO_PortInit(GPIO, BOARD_LED_PORT);
 		initPort();
 		//DIR
-		x = (uint32_t *)(GPIO|0x2000);
+		changeDIR(0x8, 1);
+		changeDIR(0x9, 1);
+		/*x = (uint32_t *)(GPIO|0x2000);
 		pin = *x;
 		pin |= 0x3<<8;
 		*x = pin;
+		*/
 		//GPIO output
 		x = (uint32_t *)(GPIO|0x1024);
 		pin = *x;
@@ -96,10 +128,11 @@ int main(void)
 		*x = pin;
 		//CTIMER_RegisterCallBack(CTIMER, &ctimer_callback[0], kCTIMER_SingleCallback);
 		initPWM();
-		x = (uint32_t *)(0x4000C010);
+		/*x = (uint32_t *)(0x4000C010);
 		pin = *x;
 		pin = 0x8;
 		*x &= pin;
+		*/
 		while(1){
 		}
     
