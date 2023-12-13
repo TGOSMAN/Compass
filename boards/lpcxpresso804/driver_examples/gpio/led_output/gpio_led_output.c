ae79dd2eb1b4b1813f23acde57a2e544cf1248b2
TGOSMAN <thomasgosman03@outlook.com>
2023-12-13 11:31:13 +1100

    PWMdutycycle function added to enable a seperate ability to change PWM dutycycle

--- boards/lpcxpresso804/driver_examples/gpio/led_output/gpio_led_output.c
+++ boards/lpcxpresso804/driver_examples/gpio/led_output/gpio_led_output.c
@@ -65,6 +65,7 @@ This function initialises the PWM Feature by
     -   Setting the Prescaler to 11 to give a 1Mhz Clock into CTIMER0
     -   Setting MR0 to 0 to start with duty cycle at 0%
     -   Setting MR3 to 1000 to give a 1khz PWM frequency
+	- 	Changing the SWM to be 
     -   Enables the internal PWM register on MR0
 Parameters:
 -   None -> maybe add frequency
@@ -91,7 +92,7 @@ void initPWM(void) {
 
     // Configure match control register (reset on MR3, interrupt)
     *MCR = (1 << 10) | (1 << 9) ;
-    *MR0 = 900;// Set duty cycle (e.g., 0%)
+    *MR0 = 999;// Set duty cycle (e.g., 0%)
     *PWMC = 0x01;// Enable PWM on MR0
 		
     // Start the timer
@@ -101,38 +102,37 @@ void initPWM(void) {
 	*PINASSIGN4 &= 0x8; 
 	return;
 }
+////////////////////////////Brief/////////////////////////////
+/*
+Description:
+This changes the PWM that is output using MR0 by:
+    -   First calculating the 
+Parameters:
+-   None -> maybe add frequency
+Return: 
+- 	Nothing
+*/
+void pwmdutycycle(uint32_t percent){
+	uint32_t matchvalue = 0;
+	volatile uint32_t *MR0 = (volatile uint32_t *) (CTIMER+0x18);
+	matchvalue = (1000-(10*percent));// this format is to ensure the decimals don't appear and zero out
+	*MR0 |= matchvalue;
+	return;
+}
 /*!
  * @brief Main function
  */
 int main(void)
 {
-		//uint32_t *poll = 0;
 		int change = 0;
 		uint32_t pin;
 		uint32_t *x = 0;
 		uint32_t *value;
-		//GPIO_PortInit(GPIO, BOARD_LED_PORT);
 		initPort();
-		//DIR
 		changeDIR(0x8, 1);
 		changeDIR(0x9, 1);
-		/*x = (uint32_t *)(GPIO|0x2000);
-		pin = *x;
-		pin |= 0x3<<8;
-		*x = pin;
-		*/
-		//GPIO output
-		x = (uint32_t *)(GPIO|0x1024);
-		pin = *x;
-		pin |= (0x1<<9);
-		*x = pin;
-		//CTIMER_RegisterCallBack(CTIMER, &ctimer_callback[0], kCTIMER_SingleCallback);
 		initPWM();
-		/*x = (uint32_t *)(0x4000C010);
-		pin = *x;
-		pin = 0x8;
-		*x &= pin;
-		*/
+		pwmdutycycle(10);
 		while(1){
 		}
     
