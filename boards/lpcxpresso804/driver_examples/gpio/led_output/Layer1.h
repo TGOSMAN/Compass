4ac6f5d15fc1cb62a47feadc6f7b8ad2ed673f58
TGOSMAN <thomasgosman03@outlook.com>
2023-12-16 12:56:41 +1100

    changed Layer 1 Header to have new functions in it

--- boards/lpcxpresso804/driver_examples/gpio/led_output/Layer1.h
+++ boards/lpcxpresso804/driver_examples/gpio/led_output/Layer1.h
@@ -7,5 +7,8 @@ void changeDIR(uint32_t PIN, int direction);
 void initPWM(void);
 void pwmdutycycle(uint32_t percent);
 void decoder(uint32_t );
-
+void delay(uint32_t ms);
+void usartinit(void);
+void sendcharusart(uint8_t character);
+uint8_t readcharusart(void);
 #endif
