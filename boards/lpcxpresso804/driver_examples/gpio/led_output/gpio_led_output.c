4decc0338e34f17b73ee880ea5a68b4981058576
TGOSMAN <thomasgosman03@outlook.com>
2023-12-16 13:04:48 +1100

    Added in testing for delay function, blinking

--- boards/lpcxpresso804/driver_examples/gpio/led_output/gpio_led_output.c
+++ boards/lpcxpresso804/driver_examples/gpio/led_output/gpio_led_output.c
@@ -6,8 +6,14 @@
  */
 int main(void)
 {
+		uint32_t *gpiovalue;
+		gpiovalue = 0xA0002100;
 		initPort();
+		changeDIR(8,1);
+		
 		while(1){
+			*gpiovalue ^= 0x1<<8;
+			delay(1000);
 		}
     
 }
