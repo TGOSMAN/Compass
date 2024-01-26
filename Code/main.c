f23b61069585d49db4693b9b5e8d8de359ceafda
TGOSMAN <thomasgosman03@outlook.com>
2024-01-27 10:24:32 +1100

    double buffer switch added

--- Code/main.c
+++ Code/main.c
@@ -7,12 +7,18 @@
 #define MAXQUE 256
 #define BUFFERSIZE 82
 
+enum status {
+	open, //0
+	closed //1
+};
+
 
 //struct list *bufferque[256] = NULL;
 //uart ringbuffer
 
 struct uartrb{
 	uint8_t	rb[BUFFERSIZE];
+	enum status door;
 	int head;
 	int tail;
 	/* data */
@@ -32,10 +38,21 @@ void USART0_DriverIRQHandler(void){
 	//volatile uint32_t *NOT0 = (volatile uint32_t *) (0xA0002300);
 	volatile uint32_t *INTSTAT = (volatile uint32_t *) (0x40064024);
 	volatile uint32_t *RXDATA = (volatile uint32_t *) (0x40064014);	
-	 //if((*INTSTAT)&0x1){
-	buffer1.rb[buffer1.head%BUFFERSIZE] = (*RXDATA);//copy the data modulo is expensive use the algorithmic approach
-	buffer1.head += 1;
-				 //}
+	if(((*RXDATA) == '$')&&(buffer2.door == closed)){
+		buffer1.door = closed;
+		buffer2.door = open;
+	} else if(((*RXDATA) == '$')&&(buffer1.door == closed)){
+		buffer1.door = open;
+		buffer2.door = closed;
+	}
+	if(buffer1.door == open){
+		buffer1.rb[buffer1.head%BUFFERSIZE] = (*RXDATA);//copy the data modulo is expensive use the algorithmic approach
+		buffer1.head += 1;
+	} else if (buffer2.door == open){
+		buffer2.rb[buffer2.head%BUFFERSIZE] = (*RXDATA);//copy the data modulo is expensive use the algorithmic approach
+		buffer2.head += 1;
+	}
+	
 //	*NOT0 |= 0x1<<9;
 //	delay(5000);
 //	*NOT0 |= 0x1<<9;
@@ -62,13 +79,16 @@ void USART0_DriverIRQHandler(void){
 void initbuffer(void){
 	buffer1.head = 0;
 	buffer1.tail = 0;
+	buffer1.door = open;
 	buffer2.head = 0;
 	buffer2.tail = 0;
+	buffer2.door = closed;
 	return;
 }
 
 
 int main(void){
+	initbuffer();
 	struct ledorient LED;
 	volatile uint32_t *NOT0 = (volatile uint32_t *) (0xA0002300);
 	initPort();
